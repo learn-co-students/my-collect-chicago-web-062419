f947fe5129f3d24467742d18906c86f67bdeada3
github username <github email address>
2019-05-16 11:37:55 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+require 'pry'
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+def my_collect(array)
+    counter = 0
+    new_array = []
+    while counter < array.length
+        new_array.push(yield array[counter])
+        counter += 1
+    end
+
+    new_array
+end
+
+# my_collect(array) do |i|
+#     new_item = i.split(" ").first    
+#     array.push(new_item)
+#     array.shift
+# end
+    
 
 
